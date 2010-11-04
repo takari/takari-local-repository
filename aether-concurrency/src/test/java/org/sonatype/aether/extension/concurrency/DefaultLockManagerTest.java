9929438af1cdf80be9c12a402af010a4fec8def7
Benjamin Bentmann <benjamin.bentmann@udo.edu>
2010-11-04 21:20:54 +0100

    o Relicensed from ASL to EPL

--- aether-concurrency/src/test/java/org/sonatype/aether/extension/concurrency/DefaultLockManagerTest.java
+++ aether-concurrency/src/test/java/org/sonatype/aether/extension/concurrency/DefaultLockManagerTest.java
@@ -1,5 +1,13 @@
 package org.sonatype.aether.extension.concurrency;
 
+/*******************************************************************************
+ * Copyright (c) 2010 Sonatype, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
 import java.io.File;
 
 import org.junit.Before;
