0dbbc886e056c2d53eb91558a9da7c923134d9b4
Benjamin Bentmann <benjamin.bentmann@udo.edu>
2013-02-16 00:14:18 +0100

    Updated copyright year

--- src/main/java/io/tesla/aether/concurrency/LockFile.java
+++ src/main/java/io/tesla/aether/concurrency/LockFile.java
@@ -1,7 +1,7 @@
 package io.tesla.aether.concurrency;
 
 /*******************************************************************************
- * Copyright (c) 2010-2011 Sonatype, Inc.
+ * Copyright (c) 2010-2013 Sonatype, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
