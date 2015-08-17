6630e2c0dcca52be78f76edf50ab367678b2d0f9
Igor Fedorenko <ifedorenko@salesforce.com>
2015-08-17 18:58:39 -0400

    integration test with maven 3.3.1 and 3.3.3
    Signed-off-by: Igor Fedorenko <ifedorenko@salesforce.com>

--- src/test/java/io/takari/aether/localrepo/its/TakariLocalRepositoryTest.java
+++ src/test/java/io/takari/aether/localrepo/its/TakariLocalRepositoryTest.java
@@ -18,7 +18,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(MavenJUnitTestRunner.class)
-@MavenVersions({"3.2.6-SNAPSHOT"})
+@MavenVersions({"3.3.1", "3.3.3"})
 public class TakariLocalRepositoryTest {
 
   @Rule
